c1461f23b59da7226e29bea50b938521bf6a88bd
polemon <polemon@gmail.com>
2015-05-19 11:55:35 +0200

    oopsie2

--- opt_neuron/gui/addWin.py
+++ opt_neuron/gui/addWin.py
@@ -1,13 +1,13 @@
-from tkinter import *
-from tkinter import ttk
-from tkinter.filedialog import askopenfilename, askopenfile
-from tkinter.messagebox import showerror
+#from tkinter import *
+#from tkinter import ttk
+#from tkinter.filedialog import askopenfilename, askopenfile
+#from tkinter.messagebox import showerror
 
 class AddFrame:
     def __init__(self):
-        self = Toplevel(width=200, height=100)
-        self.button = Button(self, text="asd")
-        self.title("child")
+#        self = Toplevel(width=200, height=100)
+#        self.button = Button(self, text="asd")
+#        self.title("child")
 #        Frame.__init__(self)
 #        self.master.title("Add Task")
 #        self.master.columnconfigure(5, weight = 1)
