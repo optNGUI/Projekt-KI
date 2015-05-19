bc1069552e69669d586338041d47fa78fc2ccfaf
NN Bachelor2 <bachelor1@gw04.(none)>
2015-05-19 12:36:14 +0200

    hopefully last orgpush

--- opt_neuron/gui/AddFrame.py
+++ opt_neuron/gui/AddFrame.py
@@ -1,11 +1,7 @@
 from gi.repository import Gtk
 
-
 class AddFrame(Gtk.Window):
     def __init__(self):
-	self.choose_ = Gtk.Box(spacing = 6)
-	self.add(self.box)
-
-	
-
+	self.choose_Algo = Gtk.Box(spacing=6)
+	self.add(self.choose_Algo)
         print("nix")
