fec7b5670e8b9981ef0b21be7a32c8fd03a7b8b8
polemon <polemon@gmail.com>
2015-05-19 11:31:48 +0200

    tkinter removed

--- opt_neuron/gui/mainWinGtk.py
+++ opt_neuron/gui/mainWinGtk.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
 import tkinter as tk
-from tkinter import *
-from tkinter import ttk
-from tkinter.filedialog import askopenfilename, askopenfile
-from tkinter.messagebox import showerror
+#from tkinter import *
+#from tkinter import ttk
+#from tkinter.filedialog import askopenfilename, askopenfile
+#from tkinter.messagebox import showerror
 from addWin import *
 #from Tktable import tktable
 
