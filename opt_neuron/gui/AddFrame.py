4fa8e2a793bdc183302617d4170c330a200a52e3
Girth <girth@64pc23.pools.uni-luebeck.de>
2015-05-19 12:54:51 +0200

    wasnt itttt...

--- opt_neuron/gui/AddFrame.py
+++ opt_neuron/gui/AddFrame.py
@@ -2,7 +2,7 @@ from gi.repository import Gtk
 
 class AddFrame(Gtk.Window):
     def __init__(self):
-        self.Window.__init__(self, title = "choose your Algorithm"
+        Gtk.Window.__init__(self, title = "choose your Algorithm")
 
         self.choose_Algo = Gtk.Box(spacing=6)
         self.add(self.choose_Algo)
