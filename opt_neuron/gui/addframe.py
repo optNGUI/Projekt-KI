fa5ce01c184ede114f5e39ff25c75aba39cffa91
NN Bachelor2 <bachelor1@gw04.(none)>
2015-05-26 13:02:03 +0200

    sshFrame, addFrame update

--- opt_neuron/gui/addframe.py
+++ opt_neuron/gui/addframe.py
@@ -6,22 +6,51 @@ from gi.repository import Gtk
 class AddFrame(Gtk.Window):
     def __init__(self,out_queue):
         Gtk.Window.__init__(self, title = "Algorithmusauswahl")
-
-        self.chooseAlgo = Gtk.Box(spacing=6)
-        self.add(self.chooseAlgo)
+	
+	self.out_queue = out_queue
+	
+	#horizontale Box für Buttons 
+        self.buttonRow = Gtk.Box(orientation = Gtk.Orientation.HORIZONTAL, spacing=6)
+        self.add(self.buttonRow)
 
         #edit button
         self.editButton = Gtk.Button(label="Hinzufügen")
         self.editButton.connect("clicked", self.on_editButton_clicked)
-        self.chooseAlgo.pack_start(self.editButton,True,True,0)        
+        self.buttonRow.pack_start(self.editButton,True,True,0)        
         #quit button
         self.quitButton = Gtk.Button(label="Abbrechen")
         self.quitButton.connect("clicked", self.on_quitButton_clicked)
-        self.chooseAlgo.pack_start(self.quitButton,True,True,0) 
+        self.buttonRow.pack_start(self.quitButton,True,True,0) 
         
-        #scrollbar for choosing algorithm
-        algoScrollbar = Gtk.Scrollbar(orientation = Gtk.Orientation.VERTICAL, adjustment = layout.get_vadjustment())
-        grid.attach(algoScrollbar,1,1,0,0)
+	#horizontale Box zur Algo/Paramauswahl
+	self.paramBox = Gtk.Box(orientation = Gtk.Orientation.HORIZONTAL, spacing = 6)
+
+        #scrollbar mit Algorithmen aus der config
+	#TODO: import algorithms from config, read in
+        self.algoScrollbar = Gtk.Scrollbar(orientation = Gtk.Orientation.VERTICAL, adjustment = layout.get_vadjustment())
+	self.paramBox.pack_start(self.algoScrollbar, True, True, 0)
+	
+	#box für die gewünschten Parameter
+	self.chooseParams = Gtk.Box(spacing = 6)
+	self.paramBox.pack_start(self.chooseParams, True, True, 0)
+
+	#Entries für die Parameter
+	#TODO: Parameter spezifisch für jeden möglichen Algo?!
+	self.param1 = Gtk.Entry()
+	self.param1.set_text("param1")
+	chooseParams.pack_start(self.param1, True, True, 0)
+
+	self.param2 = Gtk.Entry()
+	self.param2.set_text("param2")
+	chooseParams.pack_start(self.param2, True, True, 0)
+
+	self.param3 = Gtk.Entry()
+	self.param3.set_text("param3")
+	chooseParams.pack_start(self.param3, True, True, 0)
+
+	self.param4 = Gtk.Entry()
+	self.param4.set_text("param4")
+	chooseParams.pack_start(self.param4, True, True, 0)
 
     def on_editButton_clicked(self,widget,out_queue):
         #TODO: send messages to queue, fill algo+params in table
@@ -29,5 +58,7 @@ class AddFrame(Gtk.Window):
 
     def on_quitButton_clicked(self,widget):
         #closes frame without saving anything
+	self.connect("delete_event",self.close_call)
         self.destroy()
+	
         
