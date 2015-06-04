037a1eea61667fead17cf8f0b327074ccf04d45e
loqust <moritz.dannehl@informatik.uni-luebeck.de>
2015-06-04 09:59:01 +0200

    Added start_net

--- opt_neuron/core/net.py
+++ opt_neuron/core/net.py
@@ -16,3 +16,11 @@ def call(host,command):
     #tmp = ssh.communicate('ls -la')
     #ssh.communicate(bytes('exit', 'utf-8'))
     #print(str(tmp))
+
+# TODO: Annotation: Cache similar calls
+def start_net(arg0, arg1, arg2, arg3):
+    """Starts the net with the four parameters and
+    returns the fitness value calculated by the
+    analysis script.
+    """
+    return 0
