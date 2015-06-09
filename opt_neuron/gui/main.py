be67b5a68c685b70360bd8c268b13ac274665070
Chris Girth <621654@64pc310.pools.uni-luebeck.de>
2015-06-09 12:15:14 +0200

    ...

--- opt_neuron/gui/main.py
+++ opt_neuron/gui/main.py
@@ -10,7 +10,7 @@ def main(in_queue, out_queue):
     mf.connect("delete-event", Gtk.main_quit)
     mf.show_all()
 
-    af = addframe.AddFrame(out_queue)
+    af = addframe.AddFrame(in_queue,out_queue)
     af.connect("delete-event", Gtk.main_quit)
     af.show_all()
 
